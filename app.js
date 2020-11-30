75d4a8274043a2afe4ea35bb2c8927a8d715283c
John LaCroix <john.lacroix@asuresoftware.com>
2020-11-30 16:32:24 -0500

    edited for a second try

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello John LaCroix!'));
+app.get('/', (req, res) => res.send('Hello John LaCroix, second attempt!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
